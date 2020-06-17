5be5e2114aba844b2191a94b8938303ae69aba6b
qstore2 <51914096+qstore2@users.noreply.github.com>
2020-06-17 19:12:09 +0800

    1st page use cover.jpg

--- vitalsource.sh
+++ vitalsource.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 BOOK=$1
-STARTPAGE=0001
+STARTPAGE=0000
 ENDPAGE=2000
 
 mkdir -p "$BOOK"
 
-code=`curl -s -b cookies.txt -c cookies.txt "https://jigsaw.vitalsource.com/books/$BOOK/content/image/cover.jpg" -o "$BOOK/0000.jpg" -w "%{http_code}"`
-if [ $code -ne 200 ]; then
-  rm -f "$BOOK/0000.jpg"
-fi
 
 for num in $(seq -w $STARTPAGE $ENDPAGE); do
   echo Trying page $num...
-  code=`curl -s -b cookies.txt -c cookies.txt "https://jigsaw.vitalsource.com/books/$BOOK/content/image/$num.jpg" -o "$BOOK/$num.jpg" -w "%{http_code}"`
+  page=$num
+  if [ $num -eq 0000 ]; then page=cover; fi
+  code=`curl -s -b cookies.txt -c cookies.txt "https://jigsaw.vitalsource.com/books/$BOOK/content/image/$page.jpg" -o "$BOOK/$num.jpg" -w "%{http_code}"`
   if [ $code -ne 200 ]; then
     rm -f "$BOOK/$num.jpg"
 	break
